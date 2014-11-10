7403bc538b6ef85d594c160564b8ff1e34f217e9
Ye Lin Aung <me@yelinaung.com>
2014-11-11 02:10:53 +0630

    return an error msg if 404

--- app.go
+++ app.go
@@ -13,14 +13,19 @@ func returnJSON(w http.ResponseWriter, r *http.Request) {
 	url := strings.Replace(r.URL.String(), "/", "", 1)
 
 	jsonFile, err := ioutil.ReadFile(url + ".json")
-	PanicIf(err)
 
 	var p data
 
 	_ = json.Unmarshal(jsonFile, &p)
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonFile)
+	if err != nil {
+		errorMsg := map[string]string{"msg": "file not found"}
+		mapB, _ := json.Marshal(errorMsg)
+		w.Write(mapB)
+	} else {
+		w.Write(jsonFile)
+	}
 }
 
 func PanicIf(err error) {
